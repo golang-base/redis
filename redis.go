85b1b6b0ba36c7f94f909358d8164aa5d171548e
小二狼 <appx@outlook.com>
2021-02-17 10:39:54 +0800

    up

--- redis.go
+++ redis.go
@@ -4,6 +4,8 @@ import (
 	_redis "github.com/gomodule/redigo/redis"
 )
 
+var Expire = 60 * 60 * 24 // 失效时长，秒，默认24小时
+
 type Config struct {
 	Address   string // 数据库连接字符串
 	Auth      string // 最大打开的连接数
@@ -36,4 +38,28 @@ func Init(conf *Config) {
 
 func GetConn() _redis.Conn {
 	return pool.Get()
+}
+
+func Set(key string, value string) (err error) {
+	conn := GetConn()
+	defer conn.Close()
+
+	_, err = conn.Do("SET", key, value, "EX", Expire)
+	return
+}
+
+func Get(key string) (value string, err error) {
+	conn := GetConn()
+	defer conn.Close()
+
+	value, err = _redis.String(conn.Do("GET", key))
+	return
+}
+
+func Del(key string) (err error) {
+	conn := GetConn()
+	defer conn.Close()
+
+	_, err = conn.Do("DEL", key)
+	return
 }
